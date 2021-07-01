bd7bb77307c197d92aabb0673d432fb2d794b8ff
kuznetsov.e.v <g-jazzman@yandex.ru>
2021-07-01 17:35:43 +0500

    trying

--- app/src/main/java/com/example/dstesing/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/dstesing/fragments/DetailsFragment.kt
@@ -5,39 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import com.example.dstesing.Lesson
 import com.example.dstesing.OnLessonClickListener
 import com.example.dstesing.R
 import com.example.dstesing.adapters.RecyclerViewLessonAdapter
 
-class DetailsFragment : Fragment(), OnLessonClickListener  {
+class DetailsFragment : Fragment()  {
 
-    private var num: Int = DetailsFragment().lessonClick
+    private var num: Int? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
 
-   when (num) {
-       0 -> return inflater.inflate(R.layout.fragment_details, container, false)
-       1 -> return inflater.inflate(R.layout.fragment_details2, container, false)
-       2 -> return inflater.inflate(R.layout.fragment_details3, container, false)
-   }
-        return inflater.inflate(R.layout.fragment_details, container, false)
-    }
+        num = arguments?.getInt("POSITION")
+//        Toast.makeText(context,"Position is $num", Toast.LENGTH_SHORT).show()
 
-    override fun lessonClick(lesson: Lesson, position: Int) {
-        return position
-
-    }
-
+       when (num) {
+           0 -> return inflater.inflate(R.layout.fragment_details, container, false)
+           1 -> return inflater.inflate(R.layout.fragment_details2, container, false)
+           2 -> return inflater.inflate(R.layout.fragment_details3, container, false)
+       }
+            return inflater.inflate(R.layout.fragment_details, container, false)
+        }
 }
 
-class Testoro() {
-    val x = 0
-
-    fun esss (): Int {
-        return 2
-    }
-}
--- app/src/main/java/com/example/dstesing/fragments/LessonsModule1Fragment.kt
+++ app/src/main/java/com/example/dstesing/fragments/LessonsModule1Fragment.kt
@@ -1,11 +1,16 @@
 package com.example.dstesing.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentTransaction
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -32,15 +37,16 @@ class LessonsModule1Fragment : Fragment(), OnLessonClickListener {
         return binding
     }
 
-
     override fun lessonClick(lesson: Lesson, position: Int) {
+        var bundle = Bundle()
+        bundle.putInt("POSITION", position)
 
-        when(position) {
-            0 -> findNavController().navigate(R.id.action_lessonsModule1Fragment_to_detailsFragment)
-            1 -> findNavController().navigate(R.id.action_lessonsModule1Fragment_to_detailsFragment)
-            2 -> findNavController().navigate(R.id.action_lessonsModule1Fragment_to_detailsFragment)
-        }
+        var transaction = this.parentFragmentManager.beginTransaction()
+        var fragment = DetailsFragment()
+        fragment.arguments = bundle
+        transaction.replace(R.id.lesson_recycle_layout, fragment).commit()
+
+        findNavController().navigate(R.id.action_lessonsModule1Fragment_to_detailsFragment)
+
     }
-
-
 }
